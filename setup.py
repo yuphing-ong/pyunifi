baf46cb17118dfef947675ae30183b4d29acfe6a
Caleb <finish.06@gmail.com>
2018-07-29 09:06:19 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pyunifi',
+      version='2.14',
+      description='API for Ubiquity Networks UniFi controller',
+      author='Caleb Dunn',
+      author_email='finish.06@gmail.com',
+      url='https://github.com/finish06/unifi-api',
+      packages=['pyunifi'],
+      scripts=['unifi-low-snr-reconnect', 'unifi-ls-clients',
+               'unifi-save-statistics', 'unifi-log-roaming'],
+      classifiers=[],
+      install_requires=['requests'],
+      )
